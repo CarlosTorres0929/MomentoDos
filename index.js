27f35a541d32ecb74d6416bd96aba719a6deae71
Carlos Torres <ct059489@gmail.com>
2019-10-17 20:43:49 -0500

    Ultimos retoquesitos mijo no me dio pa crear archivos ome

--- index.js
+++ index.js
@@ -22,16 +22,21 @@ app.get('/database', function (req, res) {
 
 
 app.get('/saludo/:name', function saludo(req, res) {
-    res.send('Hola   ' + req.params.name)
+    if(req.params.name < 3){
+        res.send("<center><h1>Error El nombre debe tener de 3 a 40 caracteres<h1></center>");
+    }
+    res.send('Hola   ' + req.params.name);
+   
 });
 
 app.get('/edad/:xy', function(req, res) {
     if(req.params.xy < 18){
        res.send("<center><h1>Eres Menor De Edad<h1></center>");
-    }if(req.params.xy > 18){
+    }if(req.params.xy < 150){
             res.send("<center><h1>Eres Mayor De Edad<h1></center>");
-    }
-   
+    }else{
+    res.send("<center><h1>Error el numero ingresado debe ser positivo entre 0 y 150<h1></center>");
+   }
 });
 
 
